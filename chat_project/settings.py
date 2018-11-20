a43ffc91668bb175cdf8804badaea9c2ca4237aa
kali67 <hayden.233233@gmail.com>
2018-11-20 20:38:08 +1100

    update dependancies

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -74,7 +74,7 @@ ASGI_APPLICATION = 'chat.routing.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
         },
