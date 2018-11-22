6258468a639b0443536c454ead88f8268b5c211f
kali67 <hayden.233233@gmail.com>
2018-11-22 12:58:07 +1100

    added trader api call to heroku

--- chat/middleware.py
+++ chat/middleware.py
@@ -10,6 +10,6 @@ class TokenAuthMiddleware:
     def __call__(self, scope):
         # override the call with scope passed in
         token = str(dict(scope['headers'])[b'cookie']).split(";")[2].split("=")[1].strip("'")
-        response = requests.get("http://localhost:5000/users/account", headers={
+        response = requests.get("http://trader-flask-nz.herokuapp.com//users/account", headers={
             'X-Auth': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJrZXkiOjIsImlhdCI6MTU0MjgxMTA0NH0.NruUYbFo3iixgTn0TsLmXU-rX2hIsTgTOMueEY_Recg"})
         return self.inner(dict(scope, user=response.text))
