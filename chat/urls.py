943f2e1b06f041c3325a7e26c7335306ddccd780
kali67 <hayden.233233@gmail.com>
2018-11-22 11:27:18 +1100

    fixes

--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),  # empty string
+    path('<room_name>/', views.room, name="room")
+]
